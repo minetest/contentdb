3565058eea6f2ec8f47b0afce068ed0c5e801981
rubenwardy <rw@rubenwardy.com>
2018-05-24 20:42:26 +0100

    Fix package count on home page

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -43,8 +43,10 @@ def send_upload(path):
 @app.route("/")
 @menu.register_menu(app, ".", "Home")
 def home_page():
-	packages = Package.query.filter_by(approved=True).order_by(db.desc(Package.created_at)).limit(15).all()
-	return render_template("index.html", packages=packages)
+	query = Package.query.filter_by(approved=True)
+	count = query.count()
+	packages = query.order_by(db.desc(Package.created_at)).limit(15).all()
+	return render_template("index.html", packages=packages, count=count)
 
 from . import users, githublogin, packages, sass, tasks, admin, notifications
 
