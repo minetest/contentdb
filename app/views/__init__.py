137a6928bcb247cf080012b20db05e0fd796a4a8
rubenwardy <rw@rubenwardy.com>
2018-12-22 21:41:30 +0000

    Replace Popular with Top Mods

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -49,11 +49,11 @@ def home_page():
 	query   = Package.query.filter_by(approved=True, soft_deleted=False)
 	count   = query.count()
 	new     = query.order_by(db.desc(Package.created_at)).limit(8).all()
-	popular = query.order_by(db.desc(Package.score)).limit(8).all()
+	pop_mod = query.filter_by(type=PackageType.MOD).order_by(db.desc(Package.score)).limit(8).all()
 	pop_gam = query.filter_by(type=PackageType.GAME).order_by(db.desc(Package.score)).limit(4).all()
 	pop_txp = query.filter_by(type=PackageType.TXP).order_by(db.desc(Package.score)).limit(4).all()
 	return render_template("index.html", count=count, \
-			new=new, popular=popular, pop_txp=pop_txp, pop_gam=pop_gam)
+			new=new, pop_mod=pop_mod, pop_txp=pop_txp, pop_gam=pop_gam)
 
 from . import users, githublogin, packages, meta, threads, api
 from . import tasks, admin, notifications, tagseditor, licenseseditor
