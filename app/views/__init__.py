fa0506f58a592051eaed23bfde69ccee663083f6
rubenwardy <rw@rubenwardy.com>
2018-12-24 00:11:15 +0000

    Add create date to topic list

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -33,6 +33,10 @@ def domain(url):
 	return urlparse(url).netloc
 
 @app.template_filter()
+def date(value):
+    return value.strftime("%Y-%m-%d")
+
+@app.template_filter()
 def datetime(value):
     return value.strftime("%Y-%m-%d %H:%M") + " UTC"
 
