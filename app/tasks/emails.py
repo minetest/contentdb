03eccbd56ae095392d2e160d6162185ce2cc3133
rubenwardy <rw@rubenwardy.com>
2019-09-15 18:43:22 +0100

    Fix text in emails

--- app/tasks/emails.py
+++ app/tasks/emails.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from flask import render_template
+from flask import render_template, url_for
 from flask_mail import Message
 from app import mail
 from app.tasks import celery
@@ -24,7 +24,18 @@ from app.tasks import celery
 def sendVerifyEmail(newEmail, token):
 	print("Sending verify email!")
 	msg = Message("Verify email address", recipients=[newEmail])
-	msg.body = "This is a verification email!"
+
+	msg.body = """
+			This email has been sent to you because someone (hopefully you)
+			has entered your email address as a user's email.
+
+			If it wasn't you, then just delete this email.
+
+			If this was you, then please click this link to verify the address:
+
+			{}
+		""".format(url_for('verify_email_page', token=token, _external=True))
+
 	msg.html = render_template("emails/verify.html", token=token)
 	mail.send(msg)
 
@@ -33,9 +44,7 @@ def sendEmailRaw(to, subject, text, html):
 	from flask_mail import Message
 	msg = Message(subject, recipients=to)
 
-	if text:
-		msg.body = text
-
+	msg.body = text or html
 	html = html or text
 	msg.html = render_template("emails/base.html", subject=subject, content=html)
 	mail.send(msg)
