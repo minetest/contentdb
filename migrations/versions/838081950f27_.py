e6a7df6144b1d43124306851a66554db45a4ffa5
rubenwardy <rw@rubenwardy.com>
2020-07-12 02:45:19 +0100

    Fix migration misordering

--- migrations/versions/838081950f27_.py
+++ migrations/versions/838081950f27_.py
@@ -17,13 +17,13 @@ depends_on = None
 
 
 def upgrade():
-	op.create_check_constraint("mp_name_valid", "meta_package", "name ~* '^[a-z0-9_]+$'")
-
 	op.execute("""
 		DELETE FROM provides AS t USING meta_package AS m WHERE t.metapackage_id = m.id AND NOT (m.name ~* '^[a-z0-9_]+$');
 		DELETE FROM dependency AS t USING meta_package AS m WHERE t.meta_package_id = m.id AND NOT (m.name ~* '^[a-z0-9_]+$');
 		DELETE FROM meta_package WHERE NOT (name ~* '^[a-z0-9_]+$');
 	""")
+
+	op.create_check_constraint("mp_name_valid", "meta_package", "name ~* '^[a-z0-9_]+$'")
 
 
 def downgrade():
