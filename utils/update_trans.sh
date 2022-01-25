55b5893cce760053178ec5498a06e56b02084e44
rubenwardy <rw@rubenwardy.com>
2022-01-25 21:04:39 +0000

    Update translations

--- utils/update_trans.sh
+++ utils/update_trans.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pybabel extract -F babel.cfg -k lazy_gettext -o translations/messages.pot .
-pybabel update -i translations/messages.pot -d translations
+pybabel update -i translations/messages.pot -d translations --no-fuzzy-matching
