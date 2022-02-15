e42f6b2cfa9e2e49298d98327b146bd361e62d70
rubenwardy <rw@rubenwardy.com>
2022-02-15 15:26:03 +0000

    Add 500 page

--- app/__init__.py
+++ app/__init__.py
@@ -133,6 +133,11 @@ def page_not_found(e):
 	return render_template("404.html"), 404
 
 
+@app.errorhandler(500)
+def server_error(e):
+	return render_template("500.html"), 500
+
+
 @babel.localeselector
 def get_locale():
 	if not request:
