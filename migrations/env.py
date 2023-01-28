798970d6a9727c14386c5d72e86da334026a9220
rubenwardy <rw@rubenwardy.com>
2023-01-28 17:01:32 +0000

    test

--- migrations/env.py
+++ migrations/env.py
@@ -73,8 +73,8 @@ def run_migrations_online():
     context.configure(connection=connection,
                       target_metadata=target_metadata,
                       process_revision_directives=process_revision_directives,
-                      compare_type=True,
-                      **current_app.extensions['migrate'].configure_args)
+                      **current_app.extensions['migrate'].configure_args,
+                      compare_type=True)
 
     try:
         with context.begin_transaction():
