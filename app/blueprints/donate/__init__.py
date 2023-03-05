08054e4969e509edfca9c28f328368ba21d69991
rubenwardy <rw@rubenwardy.com>
2023-03-05 17:31:21 +0000

    Add page to find packages asking for donations

---
+++ app/blueprints/donate/__init__.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+from flask_login import current_user
+
+from app.models import User, Package, PackageState, db, License
+
+bp = Blueprint("donate", __name__)
+
+
+@bp.route("/donate/")
+def donate():
+	reviewed_packages = None
+	if current_user.is_authenticated:
+		reviewed_packages = Package.query.filter(
+				Package.state == PackageState.APPROVED,
+				Package.reviews.any(author_id=current_user.id, recommends=True),
+				Package.author.has(User.donate_url.isnot(None))).order_by(db.asc(Package.title)).all()
+
+	query = Package.query.filter(Package.license.has(License.is_foss == True), Package.media_license.has(License.is_foss == True),
+			Package.state == PackageState.APPROVED, Package.author.has(User.donate_url.isnot(None))).order_by(db.desc(Package.score))
+	packages_count = query.count()
+	top_packages = query.limit(40).all()
+
+	return render_template("donate/index.html",
+			reviewed_packages=reviewed_packages, top_packages=top_packages, packages_count=packages_count)
