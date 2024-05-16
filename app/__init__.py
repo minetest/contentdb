b2e543a16a38c5bbdb06b8fb72ab37226d9eeab9
rubenwardy <rw@rubenwardy.com>
2024-05-16 17:08:57 +0100

    Reduce Sentry sample rate

--- app/__init__.py
+++ app/__init__.py
@@ -52,11 +52,11 @@ if os.getenv("SENTRY_DSN"):
 		integrations=[FlaskIntegration()],
 		# Set traces_sample_rate to 1.0 to capture 100%
 		# of transactions for performance monitoring.
-		traces_sample_rate=1.0,
+		traces_sample_rate=0.1,
 		# Set profiles_sample_rate to 1.0 to profile 100%
 		# of sampled transactions.
 		# We recommend adjusting this value in production.
-		profiles_sample_rate=1.0,
+		profiles_sample_rate=0.1,
 
 		before_send=before_send,
 	)
