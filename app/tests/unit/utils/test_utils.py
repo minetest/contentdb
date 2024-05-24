daad272e2355b5814d70ff6f1f618ba05ff42fd9
rubenwardy <rw@rubenwardy.com>
2024-05-24 16:11:25 +0100

    Add user_agent is_bot tests

--- app/tests/unit/utils/test_utils.py
+++ app/tests/unit/utils/test_utils.py
@@ -17,5 +17,17 @@
 import user_agents
 
 
+def test_web_is_not_bot():
+	assert not user_agents.parse("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0").is_bot
+	assert not user_agents.parse("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+			"Chrome/125.0.0.0 Safari/537.36").is_bot
+
+
 def test_minetest_is_not_bot():
 	assert not user_agents.parse("Minetest/5.5.1 (Linux/4.14.193+-ab49821 aarch64)").is_bot
+
+
+def test_crawlers_are_bots():
+	assert user_agents.parse("Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, "
+			"like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; "
+			"+http://www.google.com/bot.html)").is_bot
